adcbc7812eb42cf59749c04d05eb9dec149fb7bf
Stefan Petcu <petcu.stephan@gmail.com>
2017-06-11 20:25:06 +0200

    Update routes; add view slug action.

--- src/OpenClassroomsCourse/PlatformBundle/Controller/AdvertController.php
+++ src/OpenClassroomsCourse/PlatformBundle/Controller/AdvertController.php
@@ -5,10 +5,16 @@ declare(strict_types=1);
 namespace OpenClassroomsCourse\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
+    /**
+     * Lists all active advertisements.
+     *
+     * @return Response
+     */
     public function indexAction(): Response
     {
         $content = $this
@@ -18,12 +24,46 @@ class AdvertController extends Controller
         return new Response($content);
     }
 
-    public function viewAction($id): Response
+    /**
+     * Displays the advertisement identified by `$id`.
+     *
+     * @param int $id
+     *
+     * @return Response
+     */
+    public function viewAction(int $id): Response
     {
         $content = $this
             ->get('templating')
             ->render('OpenClassroomsCoursePlatformBundle:Advert:view.html.twig', compact('id'));
 
         return new Response($content);
+    }
+
+
+    /**
+     * Returns a list of all the advertisements `$author` has created in `$year`.
+     * The `$format` of the response can be either HTML, or XML.
+     *
+     * @param int $year
+     * @param string $author
+     * @param string $format
+     *
+     * @return Response
+     */
+    public function viewSlugAction(int $year, string $author, string $format): Response
+    {
+        if ('html' == $format) {
+            $content = $this
+                ->get('templating')
+                ->render(
+                    'OpenClassroomsCoursePlatformBundle:Advert:view_slug.html.twig',
+                    compact('year', 'author')
+                );
+
+            return new Response($content);
+        }
+
+        return new JsonResponse(compact('year', 'author'));
     }
 }
