613a57f58e44792950becdebacda92b536f20c49
Stefan Petcu <petcu.stephan@gmail.com>
2017-06-11 13:05:28 +0200

    Add PHP 7 specific functionality where relevant.
    - added declare strict_types;
    - added return types.

--- src/OpenClassroomsCourse/PlatformBundle/Controller/HelloController.php
+++ src/OpenClassroomsCourse/PlatformBundle/Controller/HelloController.php
@@ -1,12 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace OpenClassroomsCourse\PlatformBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloController
 {
-    public function indexAction()
+    public function indexAction(): Response
     {
         return new Response("Yo world, 'sup?");
     }
--- src/OpenClassroomsCourse/PlatformBundle/OpenClassroomsCoursePlatformBundle.php
+++ src/OpenClassroomsCourse/PlatformBundle/OpenClassroomsCoursePlatformBundle.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace OpenClassroomsCourse\PlatformBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
