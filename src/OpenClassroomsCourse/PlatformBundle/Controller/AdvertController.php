07e855f5b2453a6781072ef1541ea88ec6f0ff20
Stefan Petcu <petcu.stephan@gmail.com>
2017-06-11 16:41:05 +0200

    Cleanup.

--- src/OpenClassroomsCourse/PlatformBundle/Controller/AdvertController.php
+++ src/OpenClassroomsCourse/PlatformBundle/Controller/AdvertController.php
@@ -7,13 +7,13 @@ namespace OpenClassroomsCourse\PlatformBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class HelloController extends Controller
+class AdvertController extends Controller
 {
     public function indexAction(): Response
     {
         $content = $this
             ->get('templating')
-            ->render('OpenClassroomsCoursePlatformBundle:Hello:index.html.twig', ['name' => 'Stefan']);
+            ->render('OpenClassroomsCoursePlatformBundle:Advert:index.html.twig', ['name' => 'Stefan']);
 
         return new Response($content);
     }
