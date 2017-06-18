dced7430971abefbe2c556a034d67c02afd2066f
Stefan Petcu <petcu.stephan@gmail.com>
2017-06-18 14:44:03 +0200

    Add spam filter.

---
+++ src/OpenClassroomsCourse/PlatformBundle/SpamFilter/SpamFilter.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenClassroomsCourse\PlatformBundle\SpamFilter;
+
+class SpamFilter
+{
+    /**
+     * @var int The minimum length of the text.
+     */
+    private $minLength;
+
+    /**
+     * @param int $minLength
+     */
+    public function __construct(int $minLength)
+    {
+        $this->minLength = $minLength;
+    }
+
+    /**
+     * Checks if the text is spam.
+     *
+     * @param string $text
+     *
+     * @return bool
+     */
+    public function isSpam(string $text): bool
+    {
+        return strlen($text) < $this->minLength;
+    }
+}
